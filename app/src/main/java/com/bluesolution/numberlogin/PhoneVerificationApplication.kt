a1aa5b8df381b6600808e35b1b78d342c7d9d327
Juniel Garcia <junichiiro.3@gmail.com>
2021-06-25 17:55:58 +0800

    Initial commit

---
+++ app/src/main/java/com/bluesolution/numberlogin/PhoneVerificationApplication.kt
@@ -0,0 +1,8 @@
+package com.bluesolution.numberlogin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PhoneVerificationApplication: Application() {
+}
---
+++ app/src/main/java/com/bluesolution/numberlogin/numberinput/NumberInputViewModel.kt
@@ -0,0 +1,9 @@
+package com.bluesolution.numberlogin.numberinput
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class NumberInputViewModel @ViewModelInject constructor(
+
+): ViewModel() {
+}
